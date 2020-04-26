aa85ca60f0432f22938f0b210d4c4d4db404b33a
Dzobko Ruslan <Ruslan_Dzobko@epam.com>
2020-04-26 10:40:16 +0300

    Initial

--- Function/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Function
-{
-    public class Class1
-    {
-    }
-}
---
+++ Function/Function.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Function
+{
+    public enum SortOrder { Ascending, Descending }
+    public static class Function
+    {
+        //TODO :Define public static method 'IsSorted'  that indicate  correctness of sorting array with a given sort order .
+
+
+        // TODO :Define public static  method 'Transform' -  that will increase each array element by its index, if array IsSorted in  SortOrder 
+
+
+        //TODO :Define public static  method 'MultArithmeticElements' that calculate and return   multiply   for  first n members of   arifmetic  progression 
+
+
+        //TODO :Define public static method 'SumGeometricElements'  that calculate and return   sum for members of geometric progression ,while  element is smaller than limit value  
+
+    }
+}
