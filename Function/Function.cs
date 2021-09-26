d6267b7b235f17f54659b03506fc79141e5d0d7f
Yevhenii Klimov <24evgeniy03@gmail.com>
2021-09-26 07:41:08 +0000

    Update Function.cs

--- Function/Function.cs
+++ Function/Function.cs
@@ -1,20 +1,68 @@
-﻿using System;
+using System;
 
 namespace Function
 {
     public enum SortOrder { Ascending, Descending }
     public static class Function
     {
-        //TODO :Define public static method 'IsSorted'  that indicate  correctness of sorting array with a given sort order .The values should be passed into the method in such order : array ,order. 
+        public static bool IsSorted(int[] array, SortOrder order)
+        {
+            bool isSorted = false;
+            switch (order)
+            {
+                case SortOrder.Ascending:
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        if (i > 0)
+                        {
+                            isSorted = array[i] >= array[i - 1];
+                            if (!isSorted) return false;
+                        }
+                    }
+                    break;
+                case SortOrder.Descending:
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        if (i > 0)
+                        {
+                            isSorted = array[i] <= array[i - 1];
+                            if (!isSorted) return false;
+                        }
+                    }
+                    break;
+            }
+            return isSorted;
+        }
 
+        public static void Transform(int[] array, SortOrder order)
+        {
+            if (IsSorted(array, order))
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] += i;
+                }
+        }
 
-        // TODO :Define public static  method 'Transform' -  that will increase each array element by its index, if array IsSorted in  SortOrder  . The values should be passed into the method in such order : array ,order .
+        public static double MultArithmeticElements(double a, double t, int n)
+        {
+            double result = 1;
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0)
+                    result = a;
+                else result *= (a += t);
+            }
+            return result;
+        }
 
-
-        //TODO :Define public static  method 'MultArithmeticElements' that calculate and return   multiply   for  first n members of arifmetic  progression . The values should be passed into the method in such order : a ,t ,n . 
-
-
-        //TODO :Define public static method 'SumGeometricElements'  that calculate and return   sum for members of geometric progression ,while  element is smaller than limit value. The values should be passed into the method in such order: a ,t ,alim .  
-
+        public static double SumGeometricElements(double a, double t, double alim)
+        {
+            double result = 0;
+            for (double i = a; i > alim; i*= t)
+            {
+                result += i;
+            }
+            return result;
+        }
     }
 }
